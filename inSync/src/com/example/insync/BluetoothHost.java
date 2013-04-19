9dc23c52f1c7adb800bb4ae3c3bd068b5200ddb8
Brian Lam <iBriaan@gmail.com>
2013-04-18 23:24:07 -0400

    Bluetooth Transfer functionality + Sound effects
    - Changed screen layout so that it works on phones
    - Bluetooth transfer functionality finally works
    - MP3 sound is played when buttons are clicked
    
    To do:
    - Find file path/URI after file transfer

--- inSync/src/com/example/insync/BluetoothHost.java
+++ inSync/src/com/example/insync/BluetoothHost.java
@@ -1,32 +1,43 @@
 package com.example.insync;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
-
 import android.os.Bundle;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
-import android.content.ContentValues;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 
 public class BluetoothHost extends Activity {
-	String fp = "";
-	BluetoothAdapter bA = BluetoothAdapter.getDefaultAdapter();
-	Set<BluetoothDevice> pairedDevices = bA.getBondedDevices();
-	
+	private File fp;
+
+	//Global Bluetooth Adapter
+	private BluetoothAdapter bA = BluetoothAdapter.getDefaultAdapter();
+	private Set<BluetoothDevice> pairedDevices = bA.getBondedDevices();
+
+	MediaPlayer buttonClick = null;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_bluetooth_host);
 
+		//Mediaplayer for sound that is played when button is clicked
+		buttonClick = MediaPlayer.create(this, R.raw.buttonclick);
+
 		Bundle extras = getIntent().getExtras();
-		fp = extras.getString("filepath");
+		fp = new File(extras.getString("filepath"));
 
 		final TextView uriTV = (TextView) findViewById(R.id.uriDisplayTV);
 		uriTV.setText("Your selected song: " + fp);
@@ -34,6 +45,7 @@ public class BluetoothHost extends Activity {
 		final Button sendbutton = (Button) findViewById(R.id.sendfilebutton);
 		sendbutton.setOnClickListener(new OnClickListener(){
 			public void onClick(View v){
+				buttonClick.start();
 				sendFile();
 			}
 		});
@@ -60,18 +72,40 @@ public class BluetoothHost extends Activity {
 	}
 
 	public void sendFile(){
-
-		for(BluetoothDevice bt : pairedDevices){
-			ContentValues values = new ContentValues();
-			values.put(BluetoothShare.URI, "content://" + fp);
-
-			//Send Bluetooth stuff for each Address
-			values.put(BluetoothShare.DESTINATION, bt.getAddress());
-			values.put(BluetoothShare.DIRECTION, BluetoothShare.DIRECTION_OUTBOUND);
-			Long ts = System.currentTimeMillis();
-			values.put(BluetoothShare.TIMESTAMP, ts);
+		if (bA == null) {
+			// Device does not support Bluetooth
+			return;
 		}
 
+		// Bring up Android's Bluetooth Device chooser
+		Intent intent = new Intent();
+		intent.setAction(Intent.ACTION_SEND);
+		intent.setType("text/plain");
+		intent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(fp) );
+		//...	
+
+		//list of apps that can handle our intent
+		PackageManager pm = getPackageManager();
+		List<ResolveInfo> appsList = pm.queryIntentActivities( intent, 0);
+
+		if(appsList.size() > 0) {
+			// proceed
+			//select bluetooth
+			String packageName = null;
+			String className = null;
+
+			for(ResolveInfo info: appsList){
+				packageName = info.activityInfo.packageName;
+				if( packageName.equals("com.android.bluetooth")){
+					className = info.activityInfo.name;
+					break;// found
+				}
+			}
+
+			//set our intent to launch Bluetooth
+			intent.setClassName(packageName, className);
+			startActivity(intent);
+		}
 
 	}
 
