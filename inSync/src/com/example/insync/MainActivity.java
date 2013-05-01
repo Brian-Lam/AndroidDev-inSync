750ab76f9b43afe67fca12ddd976a1b6d14ac3ba
Brian Lam <iBriaan@gmail.com>
2013-05-01 00:39:00 -0400

    Attempt at Bluetooth message transfer functionality

--- inSync/src/com/example/insync/MainActivity.java
+++ inSync/src/com/example/insync/MainActivity.java
@@ -6,15 +6,19 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 	MediaPlayer buttonClick = null;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		buttonClick = MediaPlayer.create(this, R.raw.buttonclick);
+
+		final Button testStreamButton = (Button) findViewById(R.id.testButton);
 	}
 
 	@Override
@@ -23,25 +27,24 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-	
-	//Will be called when the Create Session button is clicked
-	public void createSession(View view){
+
+	// Will be called when the Create Session button is clicked
+	public void createSession(View view) {
 		buttonClick.start();
 		Intent intent = new Intent(this, CreateSession.class);
 		startActivity(intent);
 	}
-	
-	//Will be called when the Help! :( button is clicked
-	public void helpScreen(View view){
+
+	// Will be called when the Help! :( button is clicked
+	public void helpScreen(View view) {
 		buttonClick.start();
 		Intent intent = new Intent(this, HelpScreen.class);
 		startActivity(intent);
 	}
-	
-	//Will be called when the Info button is clicked
-	public void getInfo(View view){	
+
+	public void test(View view) {
 		buttonClick.start();
-		Intent intent = new Intent(this, AboutScreen.class);
+		Intent intent = new Intent(this, BluetoothGuest.class);
 		startActivity(intent);
 	}
 }
